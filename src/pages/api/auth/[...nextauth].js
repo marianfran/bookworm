60e39e313bbd5741325a42a196045c326fb009f0
marianfrancisco <mfrancisco9@my.bcit.ca>
2023-03-30 12:52:33 -0700

    add secret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,10 +1,13 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { prisma } from "../../../../server/db/client"
 
-const secret = process.env.MY_SECRET
+const secret = process.env.SECRET
 
 export const authOptions = {
   // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
